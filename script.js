dede9308f9ed3ea3e4d0467bcb2cafe3d6316cd2
arbaazalishaikh <arbaazalishaikh73@gmail.com>
2023-10-03 18:42:47 +0530

    update

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 
 const apikey = "3fd0319f310d86264bd3b8396282ae65";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=mumbai";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 
-// const searchBox = document.querySelector(".search");
-// const searchBtn = document.querySelector(".search input");
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apikey}`);
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
     var data = await response.json();
     console.log(data);
 
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".city-name").innerHTML = data.name;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "kmph";
+    
+    document.querySelector(".container").style.display = "block";
 }
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value)
+})
 
 
 
