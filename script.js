3dea03a1bc6dbe95f8959ac8f6ccba6ad17a4328
arbaazalishaikh <arbaazalishaikh73@gmail.com>
2023-10-03 11:23:13 +0530

    update

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
-const key = "3fd0319f310d86264bd3b8396282ae65";
-// const apiKey = "863242cfb2b1d357e6093d9a4df19a4b";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+const apikey = "3fd0319f310d86264bd3b8396282ae65";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=mumbai";
 
 
 // const searchBox = document.querySelector(".search");
 // const searchBtn = document.querySelector(".search input");
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apikey}`);
     var data = await response.json();
     console.log(data);
 
